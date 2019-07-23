77ffcdd387340ed82e3b16394cc5b2d46d51ed1a
Leonardo Val <leonardo.val@creatartis.com>
2019-07-23 10:58:57 -0300

    Build using Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+﻿/** Gruntfile for [@creatartis/randomness](http://github.com/LeonardoVal/randomness.js).
+*/
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	});
+
+	require('@creatartis/creatartis-grunt').config(grunt, {
+		globalName: 'randomness',
+		sourceNames: ['__prologue__',
+				'Randomness',
+				'generators/LinearCongruential', 
+				'generators/MersenneTwister',
+			'__epilogue__'],
+		deps: [],
+		jshint: { loopfunc: true, boss: true, evil: true, proto: true },
+		karma: ['Firefox', 'Chrome']
+	});
+
+	grunt.registerTask('full-test', ['test', 'karma:test_chrome']);
+	grunt.registerTask('default', ['build']);
+};
