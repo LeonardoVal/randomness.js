04c0ae0c0034050f354da6ca72e0302d9a64dcfe
Leonardo Val <leonardo.val@creatartis.com>
2019-08-25 23:14:14 -0300

    Continue Gulp build configuration
    Add tasks for:
    
    + Minification with terser without babel traspilation.
    + Documentation with JSDoc.
    + Tests with Karma.

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,34 +8,51 @@ const babel = require('gulp-babel');
 const rename = require("gulp-rename");
 const terser = require('gulp-terser');
 const eslint = require('gulp-eslint');
+const karma = require('karma');
+const jsdoc = require('gulp-jsdoc3');
 
 const PACKAGE_NAME = parsePackageName(package.name).fullName;
 
 const SOURCES = ['__prologue__',
   'Randomness',
-  'generators/LinearCongruential', 
+  'generators/LinearCongruential',
   'generators/MersenneTwister',
   '__epilogue__',
 ];
 
 gulp.task('clean', function () {
-  return gulp.src(['build/*', 'docs/jsdoc/*'], {read: false})
+  return gulp.src(['build/*', 'docs/jsdoc/*'], { read: false })
     .pipe(clean());
 });
 
-gulp.task('compile', function() {
+gulp.task('compile', function () {
   const globs = SOURCES.map((sourceFile) => `src/${sourceFile}.js`);
   return gulp.src(globs)
     .pipe(sourcemaps.init())
     .pipe(concat(`${PACKAGE_NAME}.js`))
     .pipe(sourcemaps.write('.'))
+    .pipe(rename(`${PACKAGE_NAME}-full.js`))
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('commonjs', function() {
-  return gulp.src(`build/${PACKAGE_NAME}.js`)
-    .pipe(sourcemaps.init())
-    .pipe(babel({ 
+gulp.task('es', function () {
+  return gulp.src(`build/${PACKAGE_NAME}-full.js`)
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(babel({
+      "plugins": [
+        "@babel/plugin-proposal-class-properties",
+      ]
+    }))
+    .pipe(rename(`${PACKAGE_NAME}.js`))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('commonjs', function () {
+  return gulp.src(`build/${PACKAGE_NAME}-full.js`)
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(babel({
       "plugins": [
         "@babel/plugin-transform-modules-commonjs",
         "@babel/plugin-proposal-class-properties",
@@ -47,10 +64,10 @@ gulp.task('commonjs', function() {
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('umd', function() {
-  return gulp.src(`build/${PACKAGE_NAME}.js`)
-    .pipe(sourcemaps.init())
-    .pipe(babel({ 
+gulp.task('umd', function () {
+  return gulp.src(`build/${PACKAGE_NAME}-full.js`)
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(babel({
       "plugins": [
         "@babel/plugin-transform-modules-umd",
         "@babel/plugin-proposal-class-properties",
@@ -62,13 +79,41 @@ gulp.task('umd', function() {
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src([`build/${PACKAGE_NAME}.js`])
     .pipe(eslint())
     .pipe(eslint.formatEach('compact', process.stderr))
     .pipe(eslint.failAfterError());
 });
 
-// TODO jsdoc, karma, mocha
+gulp.task('karma', function () {
+  new karma.Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
 
-exports.default = gulp.series('clean', 'compile', 'commonjs', 'umd');
+gulp.task('doc', function () {
+  const config = {
+    "source": {
+      "exclude": ["src/__prologue__.js", "src/__epilogue__.js"],
+    },
+    "sourceType": "script",
+    "opts": {
+      "template": "templates/default",
+      "encoding": "utf8",
+      "recurse": true,
+      "destination": "./docs/jsdoc",
+    },
+    "plugins": [
+      "plugins/markdown",
+    ],
+  };
+  return gulp.src(['README.md', 'src/**/*.js'], { read: false })
+    .pipe(jsdoc(config));
+});
+
+// TODO karma, mocha, perf
+
+exports.default = gulp.series('clean', 'compile', 
+  gulp.parallel('es', 'commonjs', 'umd'), 'lint', 'doc');
