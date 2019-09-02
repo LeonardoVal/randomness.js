ab0c44c33a65c0f76c1e668f91af867172b7e878
Leonardo Val <leonardo.val@creatartis.com>
2019-09-01 23:38:41 -0300

    Tidy up Gulpfile
    The Gulpfile is now linted. Much of the code was reworked
    and cleaned up. Karma and benchmarking tasks are still WIP.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,21 @@
-const package = require('./package.json');
+/* eslint-disable camelcase */
+/* eslint-disable import/no-extraneous-dependencies */
 const parsePackageName = require('parse-packagejson-name');
 const gulp = require('gulp');
-const clean = require('gulp-clean');
-const concat = require('gulp-concat');
-const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const rename = require("gulp-rename");
-const terser = require('gulp-terser');
-const eslint = require('gulp-eslint');
+const gulp_clean = require('gulp-clean');
+const gulp_concat = require('gulp-concat');
+const gulp_sourcemaps = require('gulp-sourcemaps');
+const gulp_babel = require('gulp-babel');
+const gulp_rename = require('gulp-rename');
+const gulp_terser = require('gulp-terser');
+const gulp_eslint = require('gulp-eslint');
 const karma = require('karma');
-const jsdoc = require('gulp-jsdoc3');
-const jest = require('gulp-jest').default;
+const gulp_jsdoc = require('gulp-jsdoc3');
+const gulp_jest = require('gulp-jest').default;
+const gulp_benchmark = require('gulp-benchmark');
+const packageJSON = require('./package.json');
 
-const PACKAGE_NAME = parsePackageName(package.name).fullName;
+const PACKAGE_NAME = parsePackageName(packageJSON.name).fullName;
 
 const SOURCES = ['__prologue__',
   'Randomness',
@@ -21,128 +24,135 @@ const SOURCES = ['__prologue__',
   '__epilogue__',
 ];
 
-function task_clean() {
-  return gulp.src(['build/*', 'docs/jsdoc/*'], { read: false })
-    .pipe(clean());
-}
+const tasks = {};
+exports.tasks = tasks;
 
-function task_compile() {
+tasks.clean = function clean() {
+  return gulp.src(['build/*', 'docs/jsdoc/*'], { read: false })
+    .pipe(gulp_clean());
+};
+
+tasks.concat = function concat() {
   const globs = SOURCES.map((sourceFile) => `src/${sourceFile}.js`);
   return gulp.src(globs)
-    .pipe(sourcemaps.init())
-    .pipe(concat(`${PACKAGE_NAME}.js`))
-    .pipe(sourcemaps.write('.'))
-    .pipe(rename(`${PACKAGE_NAME}-full.js`))
-    .pipe(gulp.dest('build/'));
-}
+    .pipe(gulp_sourcemaps.init())
+    .pipe(gulp_concat(`${PACKAGE_NAME}.js`));
+};
 
 // Transpilation ///////////////////////////////////////////////////////////////
 
-function task_es() {
-  return gulp.src(`build/${PACKAGE_NAME}-full.js`)
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(babel({
-      "plugins": [
-        "@babel/plugin-proposal-class-properties",
-      ]
+tasks.es8 = function es8() {
+  return tasks.concat()
+    .pipe(gulp_babel({
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+      ],
     }))
-    .pipe(rename(`${PACKAGE_NAME}.js`))
-    .pipe(terser())
-    .pipe(sourcemaps.write('.'))
+    .pipe(gulp_rename(`${PACKAGE_NAME}-es8.js`))
+    .pipe(gulp_terser())
+    .pipe(gulp_sourcemaps.write('.'))
     .pipe(gulp.dest('build/'));
-}
+};
 
-function task_commonjs() {
-  return gulp.src(`build/${PACKAGE_NAME}-full.js`)
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(babel({
-      "plugins": [
-        "@babel/plugin-transform-modules-commonjs",
-        "@babel/plugin-proposal-class-properties",
-      ]
+tasks.commonjs = function commonjs() {
+  return tasks.concat()
+    .pipe(gulp_babel({
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        '@babel/plugin-proposal-class-properties',
+      ],
     }))
-    .pipe(rename(`${PACKAGE_NAME}-common.js`))
-    .pipe(terser())
-    .pipe(sourcemaps.write('.'))
+    .pipe(gulp_rename(`${PACKAGE_NAME}-common.js`))
+    .pipe(gulp_terser())
+    .pipe(gulp_sourcemaps.write('.'))
     .pipe(gulp.dest('build/'));
-}
+};
 
-function task_umd() {
-  return gulp.src(`build/${PACKAGE_NAME}-full.js`)
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(babel({
-      "plugins": [
-        "@babel/plugin-transform-modules-umd",
-        "@babel/plugin-proposal-class-properties",
-      ]
+tasks.umd = function umd() {
+  return tasks.concat()
+    .pipe(gulp_babel({
+      plugins: [
+        '@babel/plugin-transform-modules-umd',
+        '@babel/plugin-proposal-class-properties',
+      ],
     }))
-    .pipe(rename(`${PACKAGE_NAME}-umd.js`))
-    .pipe(terser())
-    .pipe(sourcemaps.write('.'))
+    .pipe(gulp_terser())
+    .pipe(gulp_sourcemaps.write('.'))
     .pipe(gulp.dest('build/'));
-}
+};
 
-exports.task_build = gulp.series(task_clean, task_compile,
-  gulp.parallel(task_es, task_commonjs, task_umd));
+tasks.build = gulp.series(tasks.clean,
+  gulp.parallel(tasks.es8, tasks.commonjs, tasks.umd));
 
 // Linting /////////////////////////////////////////////////////////////////////
 
-function task_lint() {
-  return gulp.src([`build/${PACKAGE_NAME}.js`])
-    .pipe(eslint())
-    .pipe(eslint.formatEach('compact', process.stderr))
-    .pipe(eslint.failAfterError());
-}
+tasks.lint = function lint() {
+  return gulp.src(['src/**/*.js', 'test/specs/*.test.js'])
+    .pipe(gulp_eslint({
+      ...packageJSON.eslintConfig,
+      globals: [], // ESLint of gulp-eslint fails if `globals` is not an array.
+    }))
+    .pipe(gulp_eslint.formatEach('compact', process.stderr));
+  // FIXME .pipe(gulp_eslint.failAfterError());
+};
 
 // Testing /////////////////////////////////////////////////////////////////////
 
-function task_specs_umd() {
-  return gulp.src(`test/specs/*.js`)
-    .pipe(babel({
-      "plugins": [
-        "@babel/plugin-transform-modules-umd",
-        "@babel/plugin-proposal-class-properties",
-      ]
+tasks.specs = function specs() {
+  return gulp.src('test/specs/*.js')
+    .pipe(gulp_babel({
+      plugins: [
+        '@babel/plugin-transform-modules-umd',
+        '@babel/plugin-proposal-class-properties',
+      ],
     }))
     .pipe(gulp.dest('build/__tests__'));
-}
+};
 
-function task_karma() {
+tasks.test_karma = function test_karma(done) {
   new karma.Server({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
+    configFile: `${__dirname}/test/karma.conf.js`,
+    singleRun: true,
   }, done).start();
-}
+};
 
-function task_jest() {
+tasks.jest = function jest() {
   gulp.src('build/**/*.js', { read: false })
-    .pipe(jest(package.jest));
-}
+    .pipe(gulp_jest(packageJSON.jest));
+};
 
-exports.task_test = gulp.series(task_specs_umd, task_jest);
+tasks.test = gulp.series(tasks.specs, tasks.jest);
+
+// Benchmarking ////////////////////////////////////////////////////////////////
+
+tasks.benchmark = function benchmark() {
+  gulp.src('build/perf/*.js', { read: false })
+    .pipe(gulp_benchmark({
+      reporters: benchmark.reporters.etalon('Suite'), // FIXME
+    }));
+};
 
 // Documentation ///////////////////////////////////////////////////////////////
 
-function task_doc() {
+tasks.jsdoc = function jsdoc() {
   const config = {
-    "source": {
-      "exclude": ["src/__prologue__.js", "src/__epilogue__.js"],
+    source: {
+      exclude: ['src/__prologue__.js', 'src/__epilogue__.js'],
     },
-    "sourceType": "script",
-    "opts": {
-      "template": "templates/default",
-      "encoding": "utf8",
-      "recurse": true,
-      "destination": "./docs/jsdoc",
+    sourceType: 'script',
+    opts: {
+      template: 'templates/default',
+      encoding: 'utf8',
+      recurse: true,
+      destination: './docs/jsdoc',
     },
-    "plugins": [
-      "plugins/markdown",
+    plugins: [
+      'plugins/markdown',
     ],
   };
   return gulp.src(['README.md', 'src/**/*.js'], { read: false })
-    .pipe(jsdoc(config));
-}
+    .pipe(gulp_jsdoc(config));
+};
 
-// TODO karma, mocha, perf
-
-exports.default = gulp.series(exports.task_build, task_lint, task_doc);
+tasks.default = gulp.series(tasks.build, tasks.lint, tasks.test, tasks.jsdoc);
+exports.default = tasks.default;
