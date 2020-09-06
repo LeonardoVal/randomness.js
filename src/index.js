eafbb68ef23284806956c9947f73beacc7c2bc86
Leonardo Val <leonardo.val@creatartis.com>
2020-09-05 23:51:23 -0300

    Rework exports

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import Randomness from './Randomness';
 import LinearCongruential from './generators/LinearCongruential';
 import MersenneTwister from './generators/MersenneTwister';
 
-const id = 'randomness';
-const SERMAT = {
+export const id = 'randomness';
+export const SERMAT = {
   include: [Randomness, LinearCongruential, MersenneTwister],
 };
 
-// eslint-disable-next-line import/prefer-default-export
 export {
-  id,
-  Randomness,
   LinearCongruential,
   MersenneTwister,
-  SERMAT,
+  Randomness,
 };
